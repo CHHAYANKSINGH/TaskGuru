67b32e6dcbc1e5e7626925370e391519119c140a
Chhayank <chhayank.singh2912@gmail.com>
2025-01-18 06:46:56 +0530

    Project initialization

---
+++ TaskGuru/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TaskGuru.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
